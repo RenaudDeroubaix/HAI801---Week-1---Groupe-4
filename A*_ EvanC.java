0eb07cfe5268ec0788671d96ee149e5e9de6a690
EvanCombot <158295022+EvanCombot@users.noreply.github.com>
2024-01-31 15:09:55 +0100

    Add files via upload

--- A*_ EvanC.java
+++ A*_ EvanC.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Classe Noeud
 public class Node {
     public String id;
     public Node parent = null;
@@ -36,10 +37,7 @@ public class Node {
     }
 }
 
-
-import java.util.*;
-
-
+// Classe pour la recherche de chemin
 public class PathFinding {
 	
     public static Node aStar(Node startNode, Node goalNode){
@@ -92,6 +90,7 @@ public class PathFinding {
 	    return null;
 	}
     
+    // Fonction pour calculer la coût des chemins
     public static int pathCost(Node startNode,Node goalNode) {
     	List<String> idas=new ArrayList<>();
     	Node target=goalNode;
@@ -115,6 +114,7 @@ public class PathFinding {
     	return x;
     }
 
+    // Affichage du chemin
 	public static void printPath(Node goalNode){
 	    Node currentNode = goalNode;
 
@@ -137,145 +137,7 @@ public class PathFinding {
 	}
 }
 
-import java.util.*;
-
-public class Node {
-	public String id;
-	public Node parent = null;
-
-	public List<Edge> neighbors;
-
-	public int FCost = Integer.MAX_VALUE;
-	public int gCost = Integer.MAX_VALUE;
-	public int hCost;
-
-	Node(int h,String id){
-		this.hCost = h;
-		this.id = id;
-		this.neighbors = new ArrayList<>();
-	}
-
-	public static class Edge {
-		Edge(int cost, Node node){
-			this.cost = cost;
-			this.node = node;
-		}
-
-		public int cost;
-		public Node node;
-	}
-
-	public void addBranch(int weight, Node node){
-		Edge newEdge = new Edge(weight, node);
-		neighbors.add(newEdge);
-	}
-
-	public int calculateHeuristic(Node target){
-		return this.hCost;
-	}
-}
-
-import java.util.*;
-
-
-public class PathFinding {
-
-	public static Node aStar(Node startNode, Node goalNode){
-		List<Node> frontier = new ArrayList<>();
-		HashSet<Node> explored = new HashSet<>();
-
-		startNode.FCost = startNode.gCost + startNode.calculateHeuristic(goalNode);
-		frontier.add(startNode);
-
-		while(!frontier.isEmpty()){
-
-			Node currentNode = frontier.get(0);
-
-			for (Node eachNode : frontier) {
-				if(eachNode.FCost<currentNode.FCost)
-					currentNode=eachNode;
-			}
-
-			if(currentNode == goalNode){
-				return currentNode;
-			}
-
-			for(Node.Edge edge : currentNode.neighbors){
-				Node neighbour = edge.node;
-
-				int totalWeight = currentNode.gCost + edge.cost;
-
-				if(!frontier.contains(neighbour) && !explored.contains(neighbour)){
-					neighbour.parent = currentNode;
-					neighbour.gCost = totalWeight;
-					neighbour.FCost = neighbour.gCost + neighbour.calculateHeuristic(goalNode);
-					frontier.add(neighbour);
-				} else {
-					if(totalWeight < neighbour.gCost){
-						neighbour.parent = currentNode;
-						neighbour.gCost = totalWeight;
-						neighbour.FCost = neighbour.gCost + neighbour.calculateHeuristic(goalNode);
-
-						if(explored.contains(neighbour)){
-							explored.remove(neighbour);
-							frontier.add(neighbour);
-						}
-					}
-				}
-			}
-
-			frontier.remove(currentNode);
-			explored.add(currentNode);
-		}
-		return null;
-	}
-
-	public static int pathCost(Node startNode,Node goalNode) {
-		List<String> idas=new ArrayList<>();
-		Node target=goalNode;
-		idas.add(goalNode.id);
-		while(target!=startNode) {
-			target=target.parent;
-			idas.add(target.id);
-		}
-
-		int x=goalNode.hCost;
-
-		for (int i = idas.size()-1; i > 0; i--) {
-			for (Node.Edge edge : startNode.neighbors) {
-				if(edge.node.id.equals(idas.get(i-1))){
-					x+=edge.cost;
-					startNode=edge.node;
-				}
-
-			}
-		}
-		return x;
-	}
-
-	public static void printPath(Node goalNode){
-		Node currentNode = goalNode;
-
-		if(currentNode==null)
-			return;
-
-		List<String> ids = new ArrayList<>();
-
-		while(currentNode.parent != null){
-			ids.add(currentNode.id);
-			currentNode = currentNode.parent;
-		}
-		ids.add(currentNode.id);
-		Collections.reverse(ids);
-		String s="";
-		for(String id : ids){
-			s+=id + " -> ";
-		}
-		System.out.println("Path\t  : "+s.substring(0, s.length()-3));
-	}
-}
-
-
+// Main (fonction de test)
 public class test {
 	public static void main(String[] args) {
 
